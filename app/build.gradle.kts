332193fcdd77e6fb91aa3bb13662d80f5b1c6878
LC <19272978+lorenzus-jpeg@users.noreply.github.com>
2025-11-22 18:05:24 +0100

    Release v1.4.2_TOA-10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.teateonair.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 11
-        versionName = "v1.4.1_TOA-9"
+        versionCode = 12
+        versionName = "v1.4.2_TOA-10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
